a9a2493769867f065ffb65dadfd93c8b9e4a2036
Gmartini621 <strangedebt621@gmail.com>
2023-02-23 05:11:17 -0800

    correction

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,25 +1,14 @@
 #include "main.h"
 /**
-*print_diagonal - prints a diagonal
+*print_line - prints a straight line
 *@n:parameter
 *Return:returns nothing
 */
-void print_diagonal(int n)
+void print_line(int n)
 {
-int len, space;
-if (n > 0)
+while (n-- > 0)
 {
-for (len = 0; len < n; len++)
-{
-for (space = 0; space < len; space++)
-_putchar(' ');
-
-_putchar('\\');
-
-if (len == (n - 1))
-continue;
-_putchar('\n');
-}
+_putchar('_');
 }
 _putchar('\n');
 }
